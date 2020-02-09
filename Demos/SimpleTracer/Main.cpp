714c38058fd0a92aa06b9fe4c6ee44d45cf2dd29
刘雨培 <liuyupei951018@hotmail.com>
2020-02-10 00:40:42 +0800

    初步完成 SimpleTracer 的 demo。

--- Demos/SimpleTracer/Main.cpp
+++ Demos/SimpleTracer/Main.cpp
@@ -1,45 +1,71 @@
 ﻿#include <Rrt2/Scene.hpp>
 #include <Rrt2/Camera.hpp>
+#include <Rrt2/Geometries/TriangleMesh.hpp>
 #include <pcg_variants.h>
+#include <Rrt2/Stb/stb_image_write.h>
 #include <numbers>
 #include <ctime>
-
+#include <numbers>
 
 int main()
 {
+    using namespace rrt;
+    constexpr std::uint32_t kFilmWidth = 60;
+    constexpr std::uint32_t kFilmHeight = 60;
+    constexpr std::uint32_t kSamplesPerPixel = 4;
     const std::unique_ptr<Scene> mainScene = std::make_unique<Scene>();
-    Camera mainCamera{
-        PerspectiveParam {
-            .near = 1.0f,
-            .far = 1000.0f,
-            .fov =
-        }
-    };
+    Camera mainCamera{PerspectiveParam{
+                          .near = 1.0f,
+                          .far = 1000.0f,
+                          .fov = std::numbers::pi_v<float> / 4.0f,
+                      },
+                      glm::identity<glm::mat4>(),
+                      FilmSize{.width = kFilmWidth, .height = kFilmHeight}};
     pcg32_random_t randState;
     pcg32_srandom_r(&randState, std::time(0), (intptr_t)&randState);
-    AlignedVec<Ray> rays;
-    mainCamera.GenerateRays(randState, rays, 4);
+    constexpr float sqrt3f = std::numbers::sqrt3_v<float>;
+
+    constexpr float kTriangleZ = 30.0f;
+    // 美丽的等边三角形
     std::vector<Vec3f> positions = {
-        Vec3f{}
-    }
-    mainScene->AddGeometry(std::make_unique<TriangleMesh>()
-    for (int i = 0; i < 50; ++i)
+        Vec3f{0.0f, sqrt3f / 2.0f - 0.5f, kTriangleZ},
+        Vec3f{0.5f, -0.5f, kTriangleZ}, Vec3f{-0.5f, -0.5f, kTriangleZ}};
+
+    std::vector<TriangleIndices> indices = {{0, 1, 2}};
+    mainScene->AddTriangleMesh(
+        std::make_unique<TriangleMesh>(positions, indices));
+    mainScene->Ready();
+    std::vector<Ray> rays;
+    mainCamera.GenerateRays(randState, rays, kSamplesPerPixel);
+    std::vector<unsigned char> pixels(kFilmHeight * kFilmWidth * 4);
+    for (int i = 30; i < kFilmHeight; ++i)
     {
-        for (int j = 0; j < 50; ++j)
+        for (int j = 30; j < kFilmWidth; ++j)
         {
             Vec3f color = {};
-            for (int k = 0; k < 4; ++k)
+            for (int k = 0; k < kSamplesPerPixel; ++k)
             {
-                const Ray& ray = rays[i * j * 4 + j * 4 + k];
+                const Ray& ray = rays[i * kFilmWidth * kSamplesPerPixel +
+                                      j * kSamplesPerPixel + k];
                 const std::optional<HitRecord> hitRecord =
                     mainScene->Trace(ray, 0.0f, 10.0f);
                 if (hitRecord)
                 {
-                    color = { 1.0f, 1.0f, 1.0f };
+                    color += Vec3f{1.0f, 1.0f, 1.0f};
                     const GeometryBase* hitGeometry =
                         mainScene->GetGeometryAt(hitRecord.value().geomId);
                 }
             }
+            color /= kSamplesPerPixel;
+            const std::size_t pixelStart = i * kFilmWidth * 4 + j * 4;
+            pixels[pixelStart] = 255 * color.x;
+            pixels[pixelStart + 1] = 255 * color.y;
+            pixels[pixelStart + 2] = 255 * color.z;
+            pixels[pixelStart + 3] = 255;
         }
     }
+    const int writtenResult =
+        stbi_write_png(".\\result.png", kFilmWidth, kFilmHeight, 4,
+                       pixels.data(), kFilmWidth * kSamplesPerPixel);
+    assert(writtenResult != 0);
 }
