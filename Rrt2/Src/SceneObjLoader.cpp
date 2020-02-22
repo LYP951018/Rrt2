8299836522011237a1612d2cbce5690f722469fa
刘雨培 <liuyupei951018@hotmail.com>
2020-02-22 16:52:01 +0800

    添加 Scene obj loader

---
+++ Rrt2/Src/SceneObjLoader.cpp
@@ -0,0 +1,64 @@
+#include <Rrt2/SceneObjLoader.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <gsl/gsl_util>
+#include <gsl/span>
+#include <Rrt2/Geometries/TriangleMesh.hpp>
+
+namespace rrt
+{
+    std::unique_ptr<TriangleMesh> MeshFromShape(const aiMesh& assimpMesh)
+    {
+        gsl::span<const aiFace> modelFaces =
+            gsl::make_span(assimpMesh.mFaces, assimpMesh.mNumFaces);
+
+        std::vector<TriangleIndices> meshIndices;
+        for (const aiFace& modelFace : modelFaces)
+        {
+            // triangles, right?
+            assert(modelFace.mNumIndices == 3);
+            // 目前只支持 16 位的 index
+            meshIndices.push_back(TriangleIndices{
+                gsl::narrow_cast<std::uint16_t>(modelFace.mIndices[0]),
+                gsl::narrow_cast<std::uint16_t>(modelFace.mIndices[1]),
+                gsl::narrow_cast<std::uint16_t>(modelFace.mIndices[2]),
+            });
+        }
+
+        gsl::span<const aiVector3D> modelVertices =
+            gsl::make_span(assimpMesh.mVertices, assimpMesh.mNumVertices);
+        std::vector<Vec3f> meshVertices;
+
+        for (const aiVector3D& modelVertex : modelVertices)
+        {
+            meshVertices.push_back(
+                Vec3f{modelVertex.x, modelVertex.y, modelVertex.z});
+        }
+
+        std::unique_ptr<TriangleMesh> triangleMesh =
+            std::make_unique<TriangleMesh>(std::move(meshVertices),
+                                           std::move(meshIndices));
+        triangleMesh->SetName(std::string{assimpMesh.mName.C_Str()});
+        return triangleMesh;
+    }
+
+    bool AddMeshesFromObj(Scene& scene, const char* objFilePath)
+    {
+        using namespace Assimp;
+        Importer importer;
+        const aiScene* modelScene =
+            importer.ReadFile(objFilePath, aiProcess_Triangulate);
+        if (!modelScene)
+        {
+            return false;
+        }
+        gsl::span<const aiMesh* const> modelMeshes =
+            gsl::make_span(modelScene->mMeshes, modelScene->mNumMeshes);
+        for (const aiMesh* modelMesh : modelMeshes)
+        {
+            scene.AddTriangleMesh(MeshFromShape(*modelMesh));
+        }
+        return true;
+    }
+} // namespace rrt
