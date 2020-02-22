857c3d93c053fd70cdcd037a33d6734d20aacfdc
刘雨培 <liuyupei951018@hotmail.com>
2020-02-22 16:45:05 +0800

    Geometry 添加名字成员

--- Rrt2/Include/Rrt2/Geometries/GeometryBase.hpp
+++ Rrt2/Include/Rrt2/Geometries/GeometryBase.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <optional>
+#include <string>
 #include "../HitRecord.hpp"
 #include "../AlignedVec.hpp"
 
@@ -18,10 +19,15 @@ namespace rrt
         virtual ~GeometryBase();
         virtual void FillPrimitiveArray(AlignedVec<PrimRefStorage>& prims) const = 0;
         virtual std::uint32_t GetPrimitiveCount() const = 0;
+
         std::uint32_t GetGeomId() const { return m_id; }
         void SetGeomId(std::uint32_t id) { m_id = id; }
 
+        void SetName(const std::string& name) { m_name = name; }
+        const std::string& GetName() const { return m_name; }
+
       private:
         std::uint32_t m_id;
+        std::string m_name;
     };
 }
