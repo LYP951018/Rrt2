71e7cd542b8c7103e6e465d3aa7e96835e0bdcfe
刘雨培 <liuyupei951018@hotmail.com>
2020-03-07 21:05:01 +0800

    添加 Hit 判断加速结构的 reference 实现。

---
+++ Rrt2/Include/Rrt2/Accelerations/ZeroAcceleration.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "AccelerationBase.hpp"
+#include <vector>
+#include "PackedTriangle.hpp"
+
+namespace rrt
+{
+    class Scene;
+
+    class ZeroAcceleration : public AccelerationBase
+    {
+      public:
+        ZeroAcceleration(const Scene* scene);
+        std::optional<HitRecord> Hit(const Ray& ray, float tMin,
+                                     float tMax) override;
+        void Build() override;
+
+      private:
+        const Scene* m_scene;
+        std::vector<PackedTriangleStorage> m_packedTriangles;
+    };
+} // namespace rrt
---
+++ Rrt2/Src/Accelerations/ZeroAcceleration.cpp
@@ -0,0 +1,45 @@
+#include "Rrt2/Accelerations/ZeroAcceleration.hpp"
+#include "Rrt2/Accelerations/Bvh.hpp"
+#include "Rrt2/Scene.hpp"
+#include "Rrt2/Geometries/GeometryBase.hpp"
+#include "Rrt2/Geometries/TriangleMesh.hpp"
+#include "Rrt2/Accelerations/PackedRay.hpp"
+
+namespace rrt
+{
+    ZeroAcceleration::ZeroAcceleration(const Scene* scene) : m_scene{scene} {}
+
+    std::optional<HitRecord> ZeroAcceleration::Hit(const Ray& ray, float tMin,
+                                                   float tMax)
+    {
+        const PackedRay packedRay{ray};
+        std::optional<HitRecord> hitRecord;
+        for (const PackedTriangleStorage& triangleStorage : m_packedTriangles)
+        {
+            const PackedTriangle packedTriangle = triangleStorage.Load();
+            const std::optional<HitRecord> childRecord =
+                packedTriangle.Hit(packedRay, ray, tMin, tMax);
+            if (!childRecord)
+                continue;
+            if (!hitRecord || hitRecord.value().t > childRecord.value().t)
+                hitRecord = childRecord;
+        }
+        return hitRecord;
+    }
+
+    void ZeroAcceleration::Build()
+    {
+        const auto& geometries = m_scene->GetGeometries();
+        for (const std::unique_ptr<GeometryBase>& geometry : geometries)
+        {
+            TriangleMesh* mesh = dynamic_cast<TriangleMesh*>(geometry.get());
+            for (std::uint32_t i = 0; i < mesh->GetPrimitiveCount(); ++i)
+            {
+                SingleTriangle triangle = mesh->GetPrimitiveAt(i);
+                PackedTriangleStorage packed;
+                packed.Set(0, triangle, i, mesh->GetGeomId());
+                m_packedTriangles.push_back(packed);
+            }
+        }
+    }
+} // namespace rrt
