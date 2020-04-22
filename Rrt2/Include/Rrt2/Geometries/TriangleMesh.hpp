a635fe02df8c9bc958662ffe813b003729069547
刘雨培 <liuyupei951018@hotmail.com>
2020-04-22 21:02:15 +0800

    调整 TriangleMesh 接口。

--- Rrt2/Include/Rrt2/Geometries/TriangleMesh.hpp
+++ Rrt2/Include/Rrt2/Geometries/TriangleMesh.hpp
@@ -4,7 +4,7 @@
 #include "../BoundingBox.hpp"
 #include "../SharedBuffer.hpp"
 #include <vector>
-#include <gsl/span>
+#include <span>
 #include <memory>
 
 namespace rrt
@@ -18,21 +18,17 @@ namespace rrt
         Vec3f v2;
     };
 
-    struct TriangleIndices
-    {
-        std::uint16_t v0, v1, v2;
-    };
-
     class TriangleMesh : public GeometryBase
     {
       public:
         static inline constexpr std::size_t kTriangleIndicesStride = 3;
 
         TriangleMesh(std::uint32_t verticesCount,
-                     std::vector<TriangleIndices> indices,
+                     std::vector<std::uint16_t> indices,
                      const Vec3f* positions, const Vec3f* normals,
                      const Vec3f* tangents);
-        SingleTriangle GetPrimitiveAt(std::uint32_t id) const;
+        std::span<const Vec3f> GetNormals() const;
+        std::span<const Vec3f> GetPositions() const;
         std::uint32_t GetPrimitiveCount() const override
         {
             return static_cast<std::uint32_t>(m_indices.size());
@@ -45,7 +41,7 @@ namespace rrt
         std::unique_ptr<Vec3f[]> m_positions;
         std::unique_ptr<Vec3f[]> m_normals;
         std::unique_ptr<Vec3f[]> m_tangents;
-        std::vector<TriangleIndices> m_indices;
+        std::vector<std::uint16_t> m_indices;
 
         void CopyVertices(const std::unique_ptr<Vec3f[]>& dest, const Vec3f* src);
         void CalculateTangentVecs();
--- Rrt2/Src/Geometries/TriangleMesh.cpp
+++ Rrt2/Src/Geometries/TriangleMesh.cpp
@@ -11,7 +11,7 @@ namespace rrt
 
 
     TriangleMesh::TriangleMesh(std::uint32_t verticesCount,
-                               std::vector<TriangleIndices> indices,
+                               std::vector<std::uint16_t> indices,
                                const Vec3f* positions, const Vec3f* normals,
                                const Vec3f* tangents)
         : m_verticesCount{verticesCount},
@@ -35,23 +35,25 @@ namespace rrt
         }
     }
 
-    SingleTriangle TriangleMesh::GetPrimitiveAt(std::uint32_t id) const
+    std::span<const Vec3f> TriangleMesh::GetNormals() const
     {
-        const TriangleIndices& indices = m_indices[id];
+        return {m_normals.get(), static_cast<std::size_t>(m_verticesCount)};
+    }
 
-        return SingleTriangle{m_positions[indices.v0], m_positions[indices.v1],
-                              m_positions[indices.v2]};
+    std::span<const Vec3f> TriangleMesh::GetPositions() const
+    {
+        return {m_positions.get(), static_cast<std::size_t>(m_verticesCount)};
     }
 
     void TriangleMesh::FillPrimitiveArray(AlignedVec<PrimRefStorage>& prims) const
     {
         std::uint32_t primId = 0;
         // TODO: loop unrolling
-        for (const TriangleIndices& indices : m_indices)
+        for (std::int32_t i = 0; i < m_indices.size(); i += 3)
         {
-            const Float4 x0 = FloatsFromMemory(&m_positions[indices.v0].x);
-            const Float4 x1 = FloatsFromMemory(&m_positions[indices.v1].x);
-            const Float4 x2 = FloatsFromMemory(&m_positions[indices.v2].x);
+            const Float4 x0 = FloatsFromMemory(&m_positions[i].x);
+            const Float4 x1 = FloatsFromMemory(&m_positions[i + 1].x);
+            const Float4 x2 = FloatsFromMemory(&m_positions[i + 2].x);
             const Float4 lower = Min(x2, Min(x0, x1));
             const Float4 upper = Max(x2, Max(x0, x1));
             BoundingBox box{lower, upper};
